e62c16abd4cd818594065ba1cb37252adf157938
Climier-code <ikowhddus@naver.com>
2024-05-10 02:28:27 +0900

    chore: 불필요한 코드 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
 import { GlobalStyle, theme } from './theme';
 import { ThemeProvider } from 'styled-components';
 import { RouterProvider } from 'react-router-dom';
@@ -15,8 +14,3 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
