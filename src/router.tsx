3cd9a020038a927e77bd4c3904ac18eca8efd511
Climier-code <ikowhddus@naver.com>
2024-05-12 01:54:36 +0900

    feat: PATHS 추가

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,12 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import Main from './components/pages/Main';
+export const PATHS = {
+  MAIN: '/',
+  SIGN_IN: '/signin',
+  SIGN_UP: '/signup',
+  TEST: '/test',
+} as const;
 
 const router = createBrowserRouter([
   {
