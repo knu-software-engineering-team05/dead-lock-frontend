733ebe8cc7233c8d5a93dbececd808093c8ae237
Climier-code <ikowhddus@naver.com>
2024-05-17 22:19:42 +0900

    feat: 종합진단결과 페이지 추가

---
+++ src/components/pages/ResultDetail/index.tsx
@@ -0,0 +1,90 @@
+import styled from 'styled-components';
+import React, { useState } from 'react';
+import { Button } from '../../common/Button';
+import { useNavigate } from 'react-router-dom';
+import { PATHS } from '../../../router';
+import { CommonWrapper, ReportBox } from '../../common/Wrapper';
+import { MOCK_RESULTS, MockResultType } from '../../../mock';
+
+const ResultDetail: React.FC = () => {
+  const navigate = useNavigate();
+
+  const [result, setResult] = useState<MockResultType>(MOCK_RESULTS[0]);
+
+  return (
+    <ResultDetailWrapper>
+      <Title>종합진단결과</Title>
+
+      <ResultCardWrapper>
+        <ResultCard>
+          <ResultCardTitle>건강점수</ResultCardTitle>
+          <ScoreBox>
+            <ScoreText>{result.score}</ScoreText>
+            <ScoreLabel>점</ScoreLabel>
+          </ScoreBox>
+        </ResultCard>
+        <ResultCard>
+          <ResultCardTitle>뇌졸중 발병률</ResultCardTitle>
+          <ScoreBox>
+            <ScoreText>{result.percentage}</ScoreText>
+            <ScoreLabel>%</ScoreLabel>
+          </ScoreBox>
+        </ResultCard>
+      </ResultCardWrapper>
+
+      <ReportBox $height="calc(100vh - 347px)">{result.diagnosis}</ReportBox>
+
+      <Button.Contained onClick={() => navigate(PATHS.SIGN_IN)} style={{ marginTop: '16px' }}>
+        생활습관 개선 프로그램 확인
+      </Button.Contained>
+    </ResultDetailWrapper>
+  );
+};
+
+const ScoreText = styled.p`
+  font-size: 64px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.primary};
+`;
+const ScoreLabel = styled.p`
+  font-size: 16px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.black};
+  padding-left: 4px;
+  padding-bottom: 10px;
+`;
+const ScoreBox = styled.div`
+  display: flex;
+  justify-content: right;
+  align-items: flex-end;
+  margin-top: 4px;
+`;
+const ResultCardTitle = styled.p`
+  font-size: 14px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.black};
+`;
+const ResultCard = styled.div`
+  width: 100%;
+  padding: 12px;
+  border-radius: 15px;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+`;
+const ResultCardWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 8px;
+  margin: 32px 0 16px;
+`;
+const Title = styled.p`
+  text-align: center;
+  font-size: 24px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.black};
+`;
+const ResultDetailWrapper = styled(CommonWrapper)`
+  padding: 40px 32px;
+`;
+
+export default ResultDetail;
