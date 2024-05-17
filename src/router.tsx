7e13af88bd84d6a6dccd0736dbac4a594f380170
Climier-code <ikowhddus@naver.com>
2024-05-17 22:20:23 +0900

    feat: router 적용

--- src/router.tsx
+++ src/router.tsx
@@ -4,11 +4,17 @@ import App from './App';
 import Main from './components/pages/Main';
 import SignUp from './components/pages/SignUp';
 import SignIn from './components/pages/SignIn';
+import ResultList from './components/pages/ResultList';
+import ResultDetail from './components/pages/ResultDetail';
+import HelpfulPrograms from './components/pages/HelpfulPrograms';
 
 export const PATHS = {
   MAIN: '/',
   SIGN_IN: '/signin',
   SIGN_UP: '/signup',
+  RESULT_LIST: '/results',
+  RESULT_DETAIL: '/results/:id',
+  SUGGEST_PROGRAM: '/results/:id/programs',
   TEST: '/test',
 } as const;
 
@@ -28,6 +34,18 @@ const router = createBrowserRouter([
       {
         path: PATHS.SIGN_UP,
         element: <SignUp />,
+      },
+      {
+        path: PATHS.RESULT_LIST,
+        element: <ResultList />,
+      },
+      {
+        path: PATHS.RESULT_DETAIL,
+        element: <ResultDetail />,
+      },
+      {
+        path: PATHS.SUGGEST_PROGRAM,
+        element: <HelpfulPrograms />,
       },
       {
         path: PATHS.TEST,
