1c20bbb120c3d574d8bfd0c9b998a0aa8b3c6ee1
Climier-code <ikowhddus@naver.com>
2024-05-17 22:19:33 +0900

    feat: 공통 report box 추가

--- src/components/common/Wrapper.tsx
+++ src/components/common/Wrapper.tsx
@@ -5,3 +5,15 @@ export const CommonWrapper = styled.section`
   height: 100vh;
 `;
 
+export const ReportBox = styled.section<{ $height: string }>`
+  border-radius: 15px;
+  background-color: #eeeeee;
+  width: 100%;
+  height: ${({ $height }) => $height};
+  padding: 20px 16px;
+
+  color: #000000;
+  font-size: 16px;
+
+  overflow-y: auto;
+`;
