87a6c781e8356e8a5913854d85f885134d01a771
Climier-code <ikowhddus@naver.com>
2024-05-29 20:27:10 +0900

    feat: API 요청 시 토큰이 만료된 경우 재로그인 유도

--- src/service/_client.ts
+++ src/service/_client.ts
@@ -5,7 +5,14 @@ const client = axios.create({ baseURL: BASE_URL + '/api/v1' });
 
 client.interceptors.response.use(
   (response) => response,
-  (error: AxiosError): Promise<never> => {
+  (error: AxiosError): Promise<never> | void => {
+    if (error.response?.status === 401) {
+      window.alert('로그인이 만료되었습니다.\n다시 로그인해주세요.');
+      window.location.href = '/signin';
+
+      return;
+    }
+
     window.alert('알 수 없는 오류가 발생되었습니다.\n잠시 후 다시 이용바랍니다.');
 
     return Promise.reject(error);
